e60b1207d83f188010f71b3b50b7846ac9c17428
Isaque <iskschluter@gmail.com>
2021-08-24 13:44:34 -0300

    package.json ready

---
+++ index.ts
@@ -0,0 +1,15 @@
+import ValidationResult from "./publicTs/isFieldValid";
+import PageElement from "./publicTs/page";
+import PageField from "./publicTs/pageField";
+import Pages from "./publicTs/pages";
+import VirtualElement from "./publicTs/virtualElement";
+
+export {
+
+    ValidationResult,
+    PageElement,
+    PageField,
+    Pages,
+    VirtualElement
+
+}
